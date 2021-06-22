a8c29f5c165201d264880d5d6bd2424a22ab9a87
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 00:28:25 +0000

    Presentation.js: changed

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,18 +4,10 @@ import _ from 'lodash';
 import { Link, withPrefix, classNames, getPageUrl } from '../utils';
 import Action from './Action';
 
-export default class Header extends React.Component {
+export default class Presentation extends React.Component {
     renderNavLinks(navLinks, pageUrl) {
         return (
-            <nav>
-                {_.map(navLinks, (action, index) => {
-                    const actionUrl = _.trim(_.get(action, 'url'), '/');
-                    const classes = classNames('nav-link', {
-                        'active': pageUrl === actionUrl
-                    });
-                    return <Action key={index} action={action} actionClass={classes} />;
-                })}
-            </nav>
+            
         );
     }
 
